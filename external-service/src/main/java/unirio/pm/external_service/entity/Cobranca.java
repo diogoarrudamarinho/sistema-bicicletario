109579d5738f406209ae38ab66cd3cdf75dc8ab7
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-06-05 17:09:58 -0300

    comecando a modelar

---
+++ external-service/src/main/java/unirio/pm/external_service/entity/Cobranca.java
@@ -0,0 +1,117 @@
+package unirio.pm.external_service.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import unirio.pm.external_service.enumerations.StatusCobranca;
+
+@Entity
+@Table(name = "tb_cobranca")
+public class Cobranca {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Valor é obrigatório")
+    private BigDecimal valor;
+
+    @NotNull(message = "Ciclista é obrigatório")
+    private Long ciclista;
+    
+    @Enumerated(EnumType.STRING)
+    private StatusCobranca status;
+    
+    private LocalDate horaSolicitacao;
+    private LocalDate horaFinalizacao;
+
+    public Cobranca(){}
+
+    public Cobranca(BigDecimal valor, Long ciclista) {
+        this.valor = valor;
+        this.ciclista = ciclista;
+        this.status = StatusCobranca.PENDENTE; 
+        this.horaSolicitacao = LocalDate.now(); 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public Long getCiclista() {
+        return ciclista;
+    }
+
+    public void setCiclista(Long ciclista) {
+        this.ciclista = ciclista;
+    }
+
+    public StatusCobranca getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusCobranca status) {
+        this.status = status;
+    }
+
+    public LocalDate getHoraSolicitacao() {
+        return horaSolicitacao;
+    }
+
+    public void setHoraSolicitacao(LocalDate horaSolicitacao) {
+        this.horaSolicitacao = horaSolicitacao;
+    }
+
+    public LocalDate getHoraFinalizacao() {
+        return horaFinalizacao;
+    }
+
+    public void setHoraFinalizacao(LocalDate horaFinalizacao) {
+        this.horaFinalizacao = horaFinalizacao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cobranca other = (Cobranca) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
