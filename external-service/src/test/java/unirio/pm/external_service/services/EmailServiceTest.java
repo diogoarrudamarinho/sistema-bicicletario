993ef39f28052301547f3acf2cf424add2125e7c
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-06-18 07:49:41 -0300

    testes pro email funcionando

--- external-service/src/test/java/unirio/pm/external_service/services/EmailServiceTest.java
+++ external-service/src/test/java/unirio/pm/external_service/services/EmailServiceTest.java
@@ -2,15 +2,14 @@ package unirio.pm.external_service.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -31,7 +30,7 @@ public class EmailServiceTest {
     @DisplayName("Should return 'Hello world' when is called")
     void helloWorld() {
         String resp = service.helloWorld();
-        assertEquals("Hello World", resp);
+        assertEquals("Hello world", resp);
     }
 
     @Test
