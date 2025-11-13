f4822466426af8bf5150d725a30d7c294a5f6fe5
Diogo Arruda <diogo.arrudamarinho@gmail.com>
2025-11-13 17:04:14 -0300

    configurando o sonar para multi modulo

---
+++ equipment-service/src/test/java/dev/unirio/equipmentservice/EquipmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.unirio.equipmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
